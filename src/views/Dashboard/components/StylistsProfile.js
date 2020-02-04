82e710100da4bb021872ab9bcbe1625caed3ab1f
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-04 14:01:39 -0500

    stylists profile is showing content

--- src/views/Dashboard/components/StylistsProfile.js
+++ src/views/Dashboard/components/StylistsProfile.js
@@ -4,17 +4,24 @@ import { connect } from "react-redux";
 import { Wrapper, Linkton, Box, Card } from "bushido-strap";
 
 const Stylists = (props) => {
-    console.log(props)
+    console.log(props.stylists)
 
-    const pushStylists = props.registerUser
-    useEffect(() => {
-        pushStylists()
-    }, [pushStylists])
+    // const pushStylists = props.registerUser
+    // useEffect(() => {
+    //     pushStylists()
+    // }, [pushStylists])
 
     return (
         <div className="stylist-wrapper">
             <div className="stylist-content">
                 <h1>Stylists Profile</h1>
+                <div key={props.stylists.id}>
+                    <img src={props.stylists.profile_url} alt="avatar picture" />
+                        <p>Name: {props.stylists.name}</p>
+                        <p>Email: {props.stylists.email}</p>
+                        <p>Location: {props.stylists.location}</p>
+                        <p>{props.stylists.profile_info}</p>
+                    </div>
             </div>
         </div>
     )
@@ -23,7 +30,8 @@ const Stylists = (props) => {
 const mapStateToProps = (state) => {
     console.log(state)
     return({
-     
+        stylists: state.registerReducer,
+        
     })
 }
 
