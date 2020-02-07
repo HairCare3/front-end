b1b07ae38d7e733bb42a36dafb3be296ccd1f2dd
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-07 05:10:26 -0500

    styled single user

--- src/views/Dashboard/components/SingleUser.js
+++ src/views/Dashboard/components/SingleUser.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import { connect } from "react-redux";
 import { fetchUserId } from "../../../store/actions/users";
+import { AppWrapper, Wrapper, Button, Card } from "bushido-strap";
+import theme from "bushido-strap/styled/theme";
 
 function User ({ fetchUserId, user, error, isFetching }){
     const { id } = useParams();
@@ -14,17 +16,24 @@ function User ({ fetchUserId, user, error, isFetching }){
         return <h2>Loading User...</h2>
     };
 
+    const picture = "https://disk.megaimg.net/643219386dce9821c2d1c9ae99c555ab";
     return (
-        <div key={user.id} id={user.id}>
-            {error && <p>{error}</p>}
-                <img src={user.profile_url}></img>
-                <h3>Username: {user.username}</h3>
-                <h4>Name: {user.name}</h4>
-                <p>Email: {user.email}</p>
-                <p>Location: {user.location}</p>
-                <p>Info: {user.profile_info}</p>
-        </div>
-    )
+        <AppWrapper bg_src={picture} m="3rem 0 3rem 0">
+            <Wrapper>
+                <Card bg={theme.green0} color="#336600">
+                <div key={user.id} id={user.id}>
+                {error && <p>{error}</p>}
+                    <img src={user.profile_url}></img>
+                    <p>Username: {user.username}</p>
+                    <p>Name: {user.name}</p>
+                    <p>Email: {user.email}</p>
+                    <p>Location: {user.location}</p>
+                    <p>Info: {user.profile_info}</p>
+                </div>
+                </Card>
+            </Wrapper>
+        </AppWrapper>
+    );
 };
 
 const mapStateToProps = state => {
